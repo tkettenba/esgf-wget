8ffa80d9cac8068c1b6f6c3c5195efa9aa5b7cdd
mauzey1 <mauzey1@llnl.gov>
2020-05-20 21:17:52 -0700

    Accept POST requests for wget

--- esgf_wget/views.py
+++ esgf_wget/views.py
@@ -1,6 +1,8 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 import urllib.request
 import urllib.parse
@@ -12,53 +14,80 @@ from .local_settings import ESGF_SOLR_SHARDS, ESGF_SOLR_URL, WGET_SCRIPT_FILE_LI
 def home(request):
     return HttpResponse('esgf-wget')
 
+@require_http_methods(['GET', 'POST'])
+@csrf_exempt
 def generate_wget_script(request):
 
-    query_url = ESGF_SOLR_URL + '/select?q=*:*&wt=json&facet=true&fq=type:File&sort=id%20asc'
-
-    if len(ESGF_SOLR_SHARDS) > 0:
-        query_url += '&shards=%s'%(','.join(ESGF_SOLR_SHARDS))
-
-    query_url += '&rows={rows}&limit={limit}&fq=dataset_id:{datasets}'
+    query_url = ESGF_SOLR_URL + '/select'
+    file_limit = WGET_SCRIPT_FILE_LIMIT
 
     # Gather dataset_ids
-    if request.GET.get('dataset_id'):
-        dataset_id_list = request.GET.getlist('dataset_id')
+    if request.method == 'POST':
+        if request.POST.get('dataset_id'):
+            dataset_id_list = request.POST.getlist('dataset_id')
+        else:
+            return HttpResponse('No datasets selected.')
+    elif request.method == 'GET':
+        if request.GET.get('dataset_id'):
+            dataset_id_list = request.GET.getlist('dataset_id')
+        else:
+            return HttpResponse('No datasets selected.')
     else:
-        return HttpResponse('No datasets selected.')
+        return HttpResponse('Request method must be POST or GET.')
 
+    # Build Solr query
     if len(dataset_id_list) == 1:
-        datasets = urllib.parse.quote_plus(dataset_id_list[0])
+        datasets_query = 'dataset_id:{}'.format(dataset_id_list[0])
     else:
-        datasets = '({})'.format(urllib.parse.quote_plus(' || '.join(dataset_id_list)))
+        datasets_query = 'dataset_id:({})'.format(' || '.join(dataset_id_list))
+
+    file_query = ['type:File', datasets_query]
+    file_attributes = ['title', 'url', 'checksum_type', 'checksum']
+    query_params = dict(q='*:*', 
+                        wt='json', 
+                        facet='true', 
+                        sort='id asc', 
+                        fl=file_attributes, 
+                        fq=file_query,
+                        limit=file_limit)
+
+    if len(ESGF_SOLR_SHARDS) > 0:
+        shards = ','.join(ESGF_SOLR_SHARDS)
+        query_params.update(dict(shards=shards))
 
     # Fetch the number of files for the query
-    query = query_url.format(rows=1, limit=WGET_SCRIPT_FILE_LIMIT, datasets=datasets)
-    with urllib.request.urlopen(query) as url:
-        results = json.loads(url.read().decode('UTF-8'))
+    count_query_params = dict(rows=1)
+    count_query_params.update(query_params)
+    count_query_encoded = urllib.parse.urlencode(count_query_params, doseq=True).encode()
+    req = urllib.request.Request(query_url, count_query_encoded)
+    with urllib.request.urlopen(req) as response:
+        results = json.loads(response.read().decode())
     num_files = results['response']['numFound']
 
     # Limit the number of files to the maximum
     wget_warn = None
     if num_files == 0:
         return HttpResponse('No files found for datasets.')
-    elif num_files > WGET_SCRIPT_FILE_LIMIT:
+    elif num_files > file_limit:
         wget_warn = 'Warning! The total number of files was {} ' \
-                    'but this script will only process {}.'.format(num_files, WGET_SCRIPT_FILE_LIMIT)
-        num_files = WGET_SCRIPT_FILE_LIMIT
+                    'but this script will only process {}.'.format(num_files, file_limit)
+        num_files = file_limit
 
     # Fetch files for the query
     file_list = []
-    query = query_url.format(rows=num_files, limit=WGET_SCRIPT_FILE_LIMIT, datasets=datasets)
-    with urllib.request.urlopen(query) as url:
-        results = json.loads(url.read().decode('UTF-8'))
+    file_query_params = dict(rows=num_files)
+    file_query_params.update(query_params)
+    file_query_encoded = urllib.parse.urlencode(file_query_params, doseq=True).encode()
+    req = urllib.request.Request(query_url, file_query_encoded)
+    with urllib.request.urlopen(req) as response:
+        results = json.loads(response.read().decode())
     for file_info in results['response']['docs']:
         filename = file_info['title']
         checksum_type = file_info['checksum_type'][0]
         checksum = file_info['checksum'][0]
         for url in file_info['url']:
             url_split = url.split('|')
-            if url_split[2] == "HTTPServer":
+            if url_split[2] == 'HTTPServer':
                 file_list.append(dict(filename=filename, 
                                         url=url_split[0], 
                                         checksum_type=checksum_type, 
@@ -67,12 +96,12 @@ def generate_wget_script(request):
 
     # Build wget script
     current_datetime = datetime.datetime.now()
-    timestamp = current_datetime.strftime("%Y/%m/%d %H:%M:%S")
+    timestamp = current_datetime.strftime('%Y/%m/%d %H:%M:%S')
 
     context = dict(timestamp=timestamp, files=file_list, warning_message=wget_warn)
     wget_script = render(request, 'wget-template.sh', context)
 
-    script_filename = current_datetime.strftime("wget-%Y%m%d%H%M%S.sh")
+    script_filename = current_datetime.strftime('wget-%Y%m%d%H%M%S.sh')
 
     response = HttpResponse(wget_script, content_type='text/x-sh')
     response['Content-Disposition'] = 'attachment; filename={}'.format(script_filename)
