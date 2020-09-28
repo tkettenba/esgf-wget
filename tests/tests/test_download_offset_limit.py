5d6d8c2caf7ebdb0a1bad980c35b48f30c86abe1
muryanto1 <muryanto1@llnl.gov>
2020-09-28 14:40:48 -0700

    adding test_offset_limit.json and test_download_offset_limit.py

--- tests/tests/test_download_offset_limit.py
+++ tests/tests/test_download_offset_limit.py
@@ -14,35 +14,36 @@ from search_utils import search_data_files
 from download_utils import get_wget_bash, run_wget_bash
 
 test_data = []
-with open("/Users/muryanto1/work/wget_api/tests/test_data/test_data.json") as f:
+with open("/Users/muryanto1/work/wget_api/tests/test_data/test_offset_limit.json") as f:
     test_data_dict = json.load(f)
     for test_descr in test_data_dict:
         a_test_dict = test_data_dict[test_descr]
-        test_tuple = tuple(a_test_dict[k] for k in ["index_node", "shards", "dataset_ids", "do_download"])
+        test_tuple = tuple(a_test_dict[k] for k in ["index_node", "shards", "dataset_ids", "do_download", "offset", "limit"])
         test_data.append(test_tuple)
 
 print("test_data: {td}".format(td=test_data))
         
-@pytest.mark.parametrize("index_node,shards,dataset_ids,do_download", test_data)
-def test_download(index_node, shards, dataset_ids, do_download):
+@pytest.mark.parametrize("index_node,shards,dataset_ids,do_download,offset,limit", test_data)
+def test_download_offset_limit(index_node, shards, dataset_ids, do_download, offset, limit):
 
     temp_dir = tempfile.mkdtemp()
     #
     # index_node is for searching
     #
     data_files = search_data_files(index_node, dataset_ids, temp_dir)
+    for f in data_files:
+        print("data file: {f}".format(f=f))
 
     wget_node = "esgf-dev2.llnl.gov"
-    ret = get_wget_bash(shards, wget_node, dataset_ids, temp_dir)
+    ret = get_wget_bash(shards, wget_node, dataset_ids, temp_dir, limit)
     assert ret == SUCCESS
 
-    ret = run_wget_bash(data_files, temp_dir, do_download=do_download)
+    ret = run_wget_bash(data_files[offset:(limit + offset if limit is not None else None)],
+                        temp_dir, do_download=do_download)
 
     shutil.rmtree(temp_dir)
     
     assert ret == SUCCESS
-
-# pytest --capture=tee-sys tests/test_download_one_dataset_id.py
 
 
 
