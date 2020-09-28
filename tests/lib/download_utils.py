f779e44edffb9ef6456107c25f0dc41f148b7fd5
muryanto1 <muryanto1@llnl.gov>
2020-09-28 14:45:40 -0700

    updated tests/lib/download_utils.py for offset and limit

--- tests/lib/download_utils.py
+++ tests/lib/download_utils.py
@@ -18,6 +18,9 @@ def get_nc_filename(full_filename):
 
 def compare_to_expected(expected_datafiles, downloaded_files):
 
+    print("INFO...number of expected datafiles: {n}".format(n=len(expected_datafiles)))
+    print("INFO...number of download files: {n}".format(n=len(downloaded_files)))
+
     if functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q, expected_datafiles, downloaded_files), True): 
         print ("PASS...Downloaded or would have downloaded file(s) match expected file(s)")
         ret = SUCCESS
@@ -26,11 +29,9 @@ def compare_to_expected(expected_datafiles, downloaded_files):
         ret = FAILURE
     return (ret)
 
-def download_data_files(shards, wget_node, dataset_ids, expected_data_files, temp_dir, expected_datafiles, do_download=True):
+def get_wget_bash(shards, wget_node, dataset_ids, temp_dir, limit=None):
 
-    # print("xxx download_data_files, shards: ", shards)
-    # print("xxx wget_node: ", wget_node)
-
+    print("xxx...get_wget_bash()...xxx")
     url = "https://{n}/wget".format(n=wget_node)
 
     params = ""
@@ -41,7 +42,11 @@ def download_data_files(shards, wget_node, dataset_ids, expected_data_files, tem
     if shards:
         shards_str = ",".join(shards)
         shards_param = " --data \"shards={s}\"".format(s=shards_str)
-        params = params + shards_param 
+        params = params + shards_param
+
+    if limit:
+        limit_param = " --data \"limit={l}\"".format(l=limit)
+        params = params + limit_param
 
     # print("xxx params: ", params)
     cmd = "curl {params} {url} -o {dir}/wget.bash".format(params=params,
@@ -49,8 +54,14 @@ def download_data_files(shards, wget_node, dataset_ids, expected_data_files, tem
                                                           dir=temp_dir)
     
     ret = run_cmd(cmd)
-    if ret != SUCCESS:
-        return(ret)
+    return ret
+
+
+def run_wget_bash(expected_datafiles, temp_dir, do_download):
+
+    print("xxx...download_data_files...xxx")
+    for f in expected_datafiles:
+        print("...f: {f}".format(f=f))
 
     if do_download:
         ret = run_cmd("bash {d}/wget.bash".format(d=temp_dir), cwd=temp_dir)
@@ -79,72 +90,6 @@ def download_data_files(shards, wget_node, dataset_ids, expected_data_files, tem
                 filename = match_obj.group(1)
                 print("WOULD HAVE DOWNLOADED {f}".format(f=filename))
                 would_have_downloaded_files.append(filename)
-            ret = compare_to_expected(expected_datafiles, would_have_downloaded_files)
+        ret = compare_to_expected(expected_datafiles, would_have_downloaded_files)
 
     return(ret)
-
-
-def download_data_filesORIG(shards, wget_node, data_node, dataset_ids, expected_data_files, temp_dir, expected_datafiles, do_download=True):
-
-    url = "https://{n}/wget".format(n=wget_node)
-
-    dataset_param = "dataset_id={d}|{dn}".format(d=dataset_id,
-                                                 dn=data_node)
-
-    params = "--data \"{ds_param}\"".format(ds_param=dataset_param)
-
-    if shards:
-        shards_param = " --data \"shards={s}\"".format(s=shards)
-        params = params + shards_param 
-
-
-    cmd = "curl {params} {url} -o {dir}/wget.bash".format(params=params,
-                                                          url=url,
-                                                          dir=temp_dir)
-
-    
-
-#    cmd = "curl --data \"dataset_id={d}|{dn}\" https://{n}/wget -o {dir}/wget.bash".format(d=dataset_id,
-#                                                                                           dn=data_node,
-#                                                                                           n=wget_node,
-#                                                                                           dir=temp_dir)
-
-
-    ret = run_cmd(cmd)
-    if ret != SUCCESS:
-        return(ret)
-
-    if do_download:
-        ret = run_cmd("bash {d}/wget.bash".format(d=temp_dir), cwd=temp_dir)
-        if ret != SUCCESS:
-            return(ret)
-
-        for f in expected_datafiles:
-            should_expect_f = "{d}/{f}".format(d=temp_dir, f=f)
-            nc_filename = get_nc_filename(should_expect_f)
-            if os.path.exists(should_expect_f):
-                print("FOUND {f}".format(f=f))
-                ret = SUCCESS
-            else:
-                print("FAIL...did not find {f}".format(f=f))
-                ret = FAILURE
-                break
-    else:
-        print("Not downloading the file")
-        would_have_downloaded_files = []
-        ret, cmd_output = run_cmd_capture_output("bash {d}/wget.bash -n".format(d=temp_dir), cwd=temp_dir)
-        if ret != SUCCESS:
-            return(ret)
-        for l in cmd_output:
-            match_obj = re.match(r'(\S+)\s+...Downloading', l)
-            if match_obj:
-                filename = match_obj.group(1)
-                print("WOULD HAVE DOWNLOADED {f}".format(f=filename))
-                would_have_downloaded_files.append(filename)
-            ret = compare_to_expected(expected_datafiles, would_have_downloaded_files)
-
-    return(ret)
-
-
-
-
